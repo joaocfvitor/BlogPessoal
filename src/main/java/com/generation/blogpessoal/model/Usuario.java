ab459919a6efe07a16fd8b400d2ef6b154a56f9a
João Vitor Fernandes <joaovitorcarvalhofernandes@hotmail.com>
2022-07-13 16:38:14 -0300

    Ajustes no backend

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -39,6 +39,8 @@ public class Usuario {
 	private String senha;
 
 	private String foto;
+	
+	private String tipo;
 
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
@@ -97,6 +99,14 @@ public class Usuario {
 
 	public void setFoto(String foto) {
 		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
 	}
 
 	public List<Postagem> getPostagem() {
